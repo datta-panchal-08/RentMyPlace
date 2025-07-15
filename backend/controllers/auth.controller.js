f58bb0c669082c4dc48472daee9c828a9b8a4fa1
dattatray panchal <dattapanchal464@gmail.com>
2025-07-15 10:24:50 +0530

    made some changes in backend

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -96,7 +96,7 @@ export const login = async (req, res) => {
             maxAge:1*24*60*60*1000,
             httpOnly:true,
             secure:true,
-            sameSite:""
+            sameSite:"None"
         });
 
         res.status(200).json({
@@ -121,7 +121,11 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
   try {
-    res.clearCookie("token");
+     res.clearCookie("token", {
+      httpOnly: true,
+      sameSite: "None",
+      secure: true,
+    })
     res.status(200).json({
       message: "Logout successful!",
       success: true,
