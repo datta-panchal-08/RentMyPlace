346cb450a24e4e77b6f215a3d1afefa4e51d9cfd
dattatray panchal <dattapanchal464@gmail.com>
2025-07-15 10:00:34 +0530

    minor changes in auth controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -95,8 +95,8 @@ export const login = async (req, res) => {
         res.cookie("token",token,{
             maxAge:1*24*60*60*1000,
             httpOnly:true,
-            secure:process.env.NODE_ENVIRONMENT = "production",
-            sameSite:"strict"
+            secure:true,
+            sameSite:""
         });
 
         res.status(200).json({
